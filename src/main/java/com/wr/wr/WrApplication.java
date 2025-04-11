0e2fe8120cd782bf399314e1f00e0f06a1b93950
yeeun0118s <yeeun0118s@naver.com>
2025-04-11 19:24:26 +0900

    초기 설정

---
+++ src/main/java/com/wr/wr/WrApplication.java
@@ -0,0 +1,13 @@
+package com.wr.wr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrApplication.class, args);
+	}
+
+}
